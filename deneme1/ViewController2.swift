b280a86ca20cc76eebf01ddb4d7b3101bd381586
erdemoden <erdemoden5@gmail.com>
2021-04-05 22:13:04 +0300

    first commit

---
+++ deneme1/ViewController2.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController2.swift
+//  deneme1
+//
+//  Created by erdem öden on 14.01.2021.
+//  Copyright © 2021 erdem öden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
